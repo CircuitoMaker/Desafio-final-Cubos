a9e5cd7971ce0b98abdb1fbba825f9379ffa0706
Talis Ribeiro <talis.rsm@gmail.com>
2023-11-24 13:20:25 -0300

    Implementação da função listar categorias finalizada

--- src/rotas.js
+++ src/rotas.js
@@ -1,18 +1,17 @@
 const express = require('express');
 const verificarLogin = require('./intermediarios/autenticacaoUsuario');
+const listarCategorias = require('./controladores/categorias');
 const rotas = express();
 
-rotas.get('/', async (req, res) => {
-    return res.json('Servidor de pé')
-})
+
 
 rotas.get('/categoria', listarCategorias);
-rotas.post('/usuario', cadastrarUsuario);
-rotas.post('login', login)
+//rotas.post('/usuario', cadastrarUsuario);
+//rotas.post('login', login)
 
 rotas.use(verificarLogin);
 
 //APAGARCOMENTÁRIO: Rotas abaixo dependem exigem usuário logado
-rotas.get('/usuario', detalharPerfilUsuario);
-rotas.put('/usuario', editarPerfilUsuario);
+//rotas.get('/usuario', detalharPerfilUsuario);
+//rotas.put('/usuario', editarPerfilUsuario);
 module.exports = rotas
