5434b1d9a94bd1e7915984d9a894d3436f9354b1
Talis Ribeiro <talis.rsm@gmail.com>
2023-11-23 22:06:56 -0300

    Insert na tabela categorias

--- src/dump.sql
+++ src/dump.sql
@@ -11,3 +11,15 @@ create table categorias
 (id serial primary key,
  descricao text not null
 );
+
+insert into categorias (descricao)
+values 
+('Informática'),
+('Celulares'),
+('Beleza e Perfumaria'),
+('Mercado'),
+('Livros e Papelaria'),
+('Brinquedos'),
+('Moda'),
+('Bebê'),
+('Games')
