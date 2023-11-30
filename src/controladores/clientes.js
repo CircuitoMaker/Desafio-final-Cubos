ccac25defb6d6636f09316785402e025bed62e03
Daniel Braga de Lima <danielbragalima.contato@gmail.com>
2023-11-30 16:51:23 -0300

    Criada função para editar clientes, sendo necessário o ID do mesmo para tal. O código verifica se e-mail e CPF não são repetidos em outros clientes do banco de dados, certifica-se de que todos os dados foram colocados e os altera. Provável alteração futura no código em relação a resposta de erro de CPF e E-mail repetidos, para maior segurança dos dados.

--- src/controladores/clientes.js
+++ src/controladores/clientes.js
@@ -37,6 +37,51 @@ const cadastrarCliente = async (req, res) => {
 
 };
 
+const editarCliente = async (req, res) => {
+
+    const { id } = req.params;
+    const { nome, email, cpf } = req.body;
+
+
+    try {
+
+        const clienteLocalizado = await pool.query('select * from clientes where id =$1', [id]);
+        if (clienteLocalizado.rowCount === 0) {
+            return res.status(404).json({ erro: 'Cliente não localizado.' })
+        }
+
+        if (!nome || !email || !cpf) {
+            return res.status(400).json({ erro: 'É necessário preencher todos os campos para realizar o cadastro do cliente.' })
+        };
+
+        const emailUnico = await pool.query('select * from clientes where email = $1 and id != $2', [email, id]);
+        if (emailUnico.rowCount > 0) {
+            return res.status(400).json({ erro: 'E-mail informado já está cadastrado.' });
+        };
+
+        const cpfUnico = await pool.query('select * from clientes where cpf = $1 and id != $2', [cpf, id]);
+        if (cpfUnico.rowCount > 0) {
+            return res.status(400).json({ erro: 'CPF informado já está cadastrado.' });
+        }
+
+
+
+        const query = ` 
+        UPDATE clientes 
+        SET nome = $1, email = $2, cpf = $3
+        WHERE id = $4
+        returning *
+        `
+        const resultado = await pool.query(query, [nome, email, cpf, id]);
+
+        res.status(200).json(resultado.rows[0]);
+
+    } catch (error) {
+        return res.status(500).json({ erro: 'Erro interno do servidor' });
+    }
+
+};
+
 
 module.exports = {
     cadastrarCliente,
