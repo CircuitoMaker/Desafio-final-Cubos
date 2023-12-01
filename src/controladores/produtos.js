43842d3fab38b795cf4ea2ba8193b8e04a5647cd
Talis Ribeiro <talis.rsm@gmail.com>
2023-12-01 19:56:58 -0300

    Implementação do JOI no cadastro de produtos

--- src/controladores/produtos.js
+++ src/controladores/produtos.js
@@ -1,145 +1,194 @@
-const pool = require('../conexao')
+const pool = require("../conexao");
+const joi = require("joi");
 
-const cadastrarProduto = async(req,res)=>{
-const {descricao,quantidade_estoque,valor,categoria_id}=req.body
+const cadastrarProduto = async (req, res) => {
+  const { descricao, quantidade_estoque, valor, categoria_id } = req.body;
 
-if(!descricao || !quantidade_estoque || !valor || !categoria_id){
-return res.status(400).json({erro:'Para cadastrar um produto, todos os campos devem ser informados.'})
-}
- try {
-     const produtoExiste = await pool.query('select * from produtos where descricao = $1',[descricao])
-    if(produtoExiste.rowCount > 0){
-        return res.status(400).json({erro:'Esse produto já foi cadastrado'})
+  const schemaProdutos = joi.object({
+    descricao: joi.string().required().messages({
+      "any.required": "O campo descrição é obrigatório",
+    }),
+    quantidade_estoque: joi.number().required().messages({
+      "any.required": "O campo de quantidade estoque é obrigatório",
+      "number.base": "Esse campo deve ser preenchido com números",
+    }),
+    valor: joi.number().min(2).required().messages({
+      "any.required": "O campo valor é obrigatório",
+      "number.base": "Esse campo deve ser preenchido com números",
+      "string.min": "A senha deve ter no mínimo 2 caracteres.",
+    }),
+    categoria_id: joi.number().required().messages({
+      "any.required": "O campo valor é obrigatório",
+      "number.base": "Esse campo deve ser preenchido com números",
+    }),
+  });
+
+  try {
+    await schemaProdutos.validateAsync({
+      descricao,
+      quantidade_estoque,
+      valor,
+      categoria_id,
+    });
+
+    const produtoExiste = await pool.query(
+      "select * from produtos where descricao = $1",
+      [descricao]
+    );
+    if (produtoExiste.rowCount > 0) {
+      return res.status(400).json({ erro: "Esse produto já foi cadastrado" });
     }
 
-    const categoriaExiste = await pool.query('select * from categorias where id = $1',[categoria_id])
-    if(categoriaExiste.rowCount < 1){
-        return res.status(400).json({erro:'Esta categoria não existe'})
+    const categoriaExiste = await pool.query(
+      "select * from categorias where id = $1",
+      [categoria_id]
+    );
+    if (categoriaExiste.rowCount < 1) {
+      return res.status(400).json({ erro: "Esta categoria não existe" });
     }
 
-const query = `
+    const query = `
     insert into produtos(descricao,quantidade_estoque,valor,categoria_id)
     values($1,$2,$3,$4) returning *
-    `
-    const {rows}= await pool.query(query,[descricao,quantidade_estoque,valor,categoria_id])
+    `;
+    const { rows } = await pool.query(query, [
+      descricao,
+      quantidade_estoque,
+      valor,
+      categoria_id,
+    ]);
 
     if (rows.length > 0) {
-    const produto  = rows[0];
-    return res.status(201).json(produto)
+      const produto = rows[0];
+      return res.status(201).json(produto);
+    }
+  } catch (error) {
+    console.log(error);
+    return res.status(400).json({ erro: error.message });
+  }
+};
+
+const editarDadosProduto = async (req, res) => {
+  const { id } = req.params;
+  const { descricao, quantidade_estoque, valor, categoria_id } = req.body;
+
+  if (!descricao || !quantidade_estoque || !valor || !categoria_id) {
+    return res
+      .status(400)
+      .json({
+        erro: "Para editar um produto, todos os campos devem ser informados.",
+      });
+  }
+  try {
+    const produtoExiste = await pool.query(
+      "select * from produtos where id = $1",
+      [id]
+    );
+    if (produtoExiste.rowCount < 1) {
+      return res.status(400).json({ erro: "Produto não encontrado!" });
     }
 
-} catch (error) {
-    return res.status(500).json({erro:'Erro interno do servidor'})
-}
-}
-
-
-const editarDadosProduto = async(req,res)=>{
-
-const {id} = req.params;
-const {descricao,quantidade_estoque,valor,categoria_id} = req.body;
-
-if(!descricao || !quantidade_estoque || !valor || !categoria_id){
-return res.status(400).json({erro:'Para editar um produto, todos os campos devem ser informados.'})
-}
- try {
-     const produtoExiste = await pool.query('select * from produtos where id = $1',[id])
-    if(produtoExiste.rowCount < 1){
-        return res.status(400).json({erro:'Produto não encontrado!'})
+    const categoriaExiste = await pool.query(
+      "select * from categorias where id = $1",
+      [categoria_id]
+    );
+    if (categoriaExiste.rowCount < 1) {
+      return res.status(400).json({ erro: "Esta categoria não existe" });
     }
 
-    const categoriaExiste = await pool.query('select * from categorias where id = $1',[categoria_id])
-    if(categoriaExiste.rowCount < 1){
-        return res.status(400).json({erro:'Esta categoria não existe'})
-    }
-
-const query = `
+    const query = `
 UPDATE produtos
 SET descricao = $1, quantidade_estoque = $2, valor = $3, categoria_id = $4
 WHERE id = $5; 
 `;
-    const {rows,rowCount} = await pool.query(query,[descricao, quantidade_estoque, valor, categoria_id, id])
+    const { rows, rowCount } = await pool.query(query, [
+      descricao,
+      quantidade_estoque,
+      valor,
+      categoria_id,
+      id,
+    ]);
 
- if (rowCount > 0) {
-    return res.status(201).json("Produto Atualizado")
+    if (rowCount > 0) {
+      return res.status(201).json("Produto Atualizado");
+    }
+  } catch (error) {
+    return res.status(500).json({ erro: "Erro interno do servidor" });
+  }
+};
+
+const listarProdutos = async (req, res) => {
+  const { categoria_id } = req.query;
+
+  try {
+    if (categoria_id) {
+      const produtoExiste = await pool.query(
+        "select * from produtos where categoria_id = $1",
+        [categoria_id]
+      );
+      if (produtoExiste.rowCount < 1) {
+        return res
+          .status(400)
+          .json({ erro: "Sem produtos cadastrados nesta categoria!" });
+      }
+      const produtoEncontrado = produtoExiste.rows;
+      return res.status(201).json(produtoEncontrado);
     }
 
-} catch (error) {
-    return res.status(500).json({erro:'Erro interno do servidor'})
-}
-}
+    const listaProdutos = await pool.query("select * from produtos");
+    const lista = listaProdutos.rows;
+    return res.status(201).json(lista);
+  } catch (error) {
+    return res.status(500).json({ erro: "Erro interno do servidor" });
+  }
+};
 
-const listarProdutos = async(req,res)=>{
-    const {categoria_id} = req.query;
- 
-try {
-    if(categoria_id){
-         const produtoExiste = await pool.query('select * from produtos where categoria_id = $1',[categoria_id])
-         if(produtoExiste.rowCount < 1){
-             return res.status(400).json({erro:'Sem produtos cadastrados nesta categoria!'})
-         }
-        const produtoEncontrado = produtoExiste.rows
-        return res.status(201).json(produtoEncontrado)
+const detalharProduto = async (req, res) => {
+  const { id } = req.params;
+
+  try {
+    if (id) {
+      const produtoExiste = await pool.query(
+        "select * from produtos where id = $1",
+        [id]
+      );
+      if (produtoExiste.rowCount < 1) {
+        return res.status(400).json({ erro: "Produto não encontrado!" });
+      }
+      const produtoEncontrado = produtoExiste.rows[0];
+      return res.status(201).json(produtoEncontrado);
     }
+  } catch (error) {
+    return res.status(500).json({ erro: "Erro interno do servidor" });
+  }
+};
 
-    const listaProdutos = await pool.query('select * from produtos');
-    const lista = listaProdutos.rows
-    return res.status(201).json(lista)
+const excluirProduto = async (req, res) => {
+  const { id } = req.params;
 
-} catch (error) {
-    return res.status(500).json({erro:'Erro interno do servidor'})
-}    
-}
-
-
-const detalharProduto = async(req,res)=>{
-    const {id} = req.params;
- 
-    try {
-        if(id){
-            const produtoExiste = await pool.query('select * from produtos where id = $1',[id])
-            if(produtoExiste.rowCount < 1){
-                return res.status(400).json({erro:'Produto não encontrado!'})
-            }
-            const produtoEncontrado = produtoExiste.rows[0]
-            return res.status(201).json(produtoEncontrado)
-    
-        }
-    
-    } catch (error) {
-        return res.status(500).json({erro:'Erro interno do servidor'})
-    }    
-}
-
-
-const excluirProduto = async(req,res)=>{
-const {id} = req.params;
-
-try { 
+  try {
     const query = `
     DELETE FROM produtos
     WHERE id = $1;
 `;
-const { rowCount } = await pool.query(query, [id]);
+    const { rowCount } = await pool.query(query, [id]);
 
-        if(rowCount > 0){
-            return res.status(200).json({erro:'Produto excluído com sucesso!'})
-        }else{
-            return res.status(404).json({ erro: 'Produto não encontrado para exclusão' });  
-        }
-
-} catch (error) {
-    return res.status(500).json({erro:'Erro interno do servidor'})  
-}
-}
-
+    if (rowCount > 0) {
+      return res.status(200).json({ erro: "Produto excluído com sucesso!" });
+    } else {
+      return res
+        .status(404)
+        .json({ erro: "Produto não encontrado para exclusão" });
+    }
+  } catch (error) {
+    return res.status(500).json({ erro: "Erro interno do servidor" });
+  }
+};
 
 module.exports = {
-    cadastrarProduto,
-    editarDadosProduto,
-    listarProdutos,
-    detalharProduto,
-    excluirProduto
-}
-
-
+  cadastrarProduto,
+  editarDadosProduto,
+  listarProdutos,
+  detalharProduto,
+  excluirProduto,
+};
